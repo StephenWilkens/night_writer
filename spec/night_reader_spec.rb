0cd80c33ca160b708ca8e8cfeb96365b4676508b
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-25 16:01:32 -0600

    Test: add test for confirmation message

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -6,4 +6,8 @@ let(:night_reader){NightReader.new}
   it 'exists' do
     expect(night_reader).to be_an(NightReader)
   end
+
+  it 'can print a confirmation message' do
+    expect(night_reader.message).to be(puts "Created #{ARGV[1]} containing #{night_reader.total_characters} characters")
+  end
 end
