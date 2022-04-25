942a0c6a0759391e4a1d8714b6cb936ee41e3fa3
Stephen Wilkens <stephenwilkens@gmail.com>
2022-04-25 16:33:47 -0600

    Feat:add total_characters and message methods. Passing tests but printing string in test.

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,7 +1,22 @@
 
 class NightReader
-
+  attr_accessor :input, :output
   def initialize
+    @input = ARGV[0]
+    @output = ARGV[1]
 
+  end
+
+  def total_characters
+    @lines = []
+    if ARGV[1] != nil
+      @lines = File.readlines(ARGV[1])
+    end
+    text = @lines.join
+    return text.length
+  end
+
+  def message
+    puts "Created #{ARGV[1]} containing #{total_characters} characters"
   end
 end
